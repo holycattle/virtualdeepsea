6dc150163f04026e4e4741d91a09282ffc29bfd7
holycattle <jlibasco@gmail.com>
2012-09-18 01:55:48 +0800

    gameobjectmanager.js and main.js

---
+++ res/src/gameobjectmanager.js
@@ -0,0 +1,61 @@
+function GameObjectManager() {
+	this.gameObjects = new Array();
+	this.lastFrame = new Date().getTime();
+	this.xScroll = 0;
+	this.yScroll = 0;
+	this.applicationManager = null; //type HTMLCanvasElement
+	this.canvas = null;
+	this.context2D = null;
+	this.backBuffer = null;
+	this.backBufferContext2D = null;
+	
+	this.startupGameObjectManager = function() {
+		g_GameObjectManager = this;
+		
+		this.canvas = document.getElementById('backgroundCanvas'); //whatever the name of the canvas is
+		this.context2D = this.canvas.getContext('2d');
+		this.backBuffer = document.createElement('backgroundCanvas'); //name of the canvas
+		this.backBuffer.width = this.canvas.width;
+		this.backBuffer.height = this.canvas.height;
+		this.backBufferContext2D = this.backBuffer.getContext('2d');
+		this.applicationManager = new ApplicationManager().startupApplicationManager();
+		setInterval(function(){g_GameObjectManager.draw();}, SECONDS_BETWEEN_FRAMES);
+		return this;
+	}
+	
+	this.draw = function() {
+		// calculate the time since the last frame
+		var thisFrame = new Date().getTime();
+		var dt = (thisFrame - this.lastFrame)/1000;
+		this.lastFrame = thisFrame;
+		
+		// clear the drawing contexts
+		this.backBufferContext2D.clearRect(0, 0, this.backBuffer.width, this.backBuffer.height);
+		this.context2D.clearRect(0, 0, this.canvas.width, this.canvas.height);
+	
+		for(x in this.gameObjects) {
+			if(this.gameObjects[x].draw) {
+				this.gameObjects[x].update(dt, this.backBufferContext2D)
+			}
+		}
+		
+		for(x in this.gameObjects) {
+			if(this.gameObjects[x].draw) {
+				this.gameObjects[x].draw(dt, this.backBufferContext2D, this.xScroll, this.yScroll);
+			}
+			
+		}
+		
+		//copy the back buffer to the displayed canvas
+		this.context2D.drawImage(this.backBuffer, 0, 0);
+	};
+	
+	this.addGameObject = function(gameObject) {
+		this.gameObjects.push(gameObject);
+		this.gameObjects.sort(function(a, b) {return a.zOrder - b.zOrder;});
+	};
+	
+	this.removeGameObject = function(gameObject) {
+		this.gameObjects.removeObject(gameObject);
+	};
+}
---
+++ res/src/main.js
@@ -0,0 +1,11 @@
+var FPS = 30;
+var SECONDS_BETWEEN_FRAMES = 1/FPS;
+var g_GameObjectManager = null;
+var g_image = new Image();
+//g_image.src = "path/to/image";
+
+window.onload = init;
+
+function init() {
+	new GameObjectManager().startupGameObjectManager();
+}
